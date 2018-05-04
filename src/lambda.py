d7c1b7dd3d7ec3738aaa977cd0fbb495ced20ef3
Severo <severo.fabricio@gmail.com>
2018-05-03 22:59:05 -0300

    Added the missing try except block

--- src/lambda.py
+++ src/lambda.py
@@ -1,5 +1,6 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table('datasetc')
@@ -7,9 +8,12 @@ table = dynamodb.Table('datasetc')
 def lambda_handler(event, context):
     document_number = event['document']
 
-    response  = table.get_item(
-        Key={'Document': document_number}
-    )
+    try:
+        response  = table.get_item(
+            Key={'Document': document_number}
+        )
+    except ClientError as e:
+        raise Exception(e.response['Error']['Message'])
 
     if 'Item' not in response:
         raise Exception('Document not found')
